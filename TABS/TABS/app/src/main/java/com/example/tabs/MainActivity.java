85e0d7409b1282a22263f766e7d45843cb9174ff
JuanRojasL <112908543+JuanRojasL@users.noreply.github.com>
2022-11-22 22:38:14 -0500

    Add files via upload

---
+++ TABS/TABS/app/src/main/java/com/example/tabs/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.tabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import Controlador.PagerController;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tab1,tab2,tab3;
+    PagerController pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tablalayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tab1 = findViewById(R.id.tabcontactos);
+        tab2 = findViewById(R.id.tabcorreos);
+        tab3 = findViewById(R.id.tabmensajes);
+
+        pagerAdapter = new PagerController(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==2){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+    }
+}
